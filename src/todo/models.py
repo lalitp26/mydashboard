bff2a952a177960e8a5df2553ab938a819b0e44e
lalitp777 <lalit.patil@medipta.com>
2018-07-28 17:20:22 +0530

    Dashboard

--- src/todo/models.py
+++ src/todo/models.py
@@ -8,6 +8,7 @@ class Project(models.Model):
 	name = models.CharField(max_length = 200)
 	description = models.TextField()
 	created_at = models.DateTimeField(auto_now = False, auto_now_add = True)
+	is_active = models.BooleanField(default= False)
 
 	def __str__(self):
 		return self.name
